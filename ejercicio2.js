4db6c3ab872c9bf8d61eff4dee6b0f440aad91bc
AndryHuipe <77870794+AndryHuipe@users.noreply.github.com>
2021-08-12 04:12:49 -0500

    Create ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,23 @@
+ unction evaluarNota(){
+    do{
+        var nota = prompt("Introduce tu nota");
+        if(Number(nota)==nota){            
+            var n = parseInt(nota);
+            if(n>0 && n<=3){
+                alert("Muy deficiente");
+            }else if(n<5){
+                alert("Insuficiente");
+            }else if(n<6){
+                alert("Suficiente");
+            }else if(n<7){
+                alert("Bien");
+            } else if(n<9){
+                alert("Muy Bien");
+            }else if(n>=9){
+                alert("Sobresaliente");
+            }
+        }else{
+            alert("Introduce un número válido");
+        }
+    }while(nota!=undefined);
+}
